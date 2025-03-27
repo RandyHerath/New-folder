1592e79409f1c301767708e250df0c4a682a583e
Randy herath <lahiruherathmhm@gmail.com>
2025-03-28 00:07:11 +1100

    first commit

---
+++ simple_script.js
@@ -0,0 +1,13 @@
+// Simple JavaScript File
+
+// Function to add two numbers
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Example usage
+let num1 = 5;
+let num2 = 7;
+
+let result = addNumbers(num1, num2);
+console.log("The sum is: " + result);
